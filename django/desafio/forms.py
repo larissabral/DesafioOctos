fce42ab8c210ad30d88d0e6c341d365cdb918713
larissabral <64551159+larissabral@users.noreply.github.com>
2020-06-19 12:29:48 -0300

    Mudança nos campos fabricante e serie
    Inclusão de choices no campo fabricante e tornar o campo serie com entradas uppercase

--- django/desafio/forms.py
+++ django/desafio/forms.py
@@ -1,6 +1,9 @@
 from django import forms
 from .models import Camera
 
+class SerieField(forms.CharField):
+    def to_python(self, value):
+        return value.upper()
 
 FABRICANTES = (
     ('', 'Selecione o fabricante...'),
@@ -12,7 +15,7 @@ FABRICANTES = (
 )
 
 class AddForm(forms.Form):
-    nome = forms.CharField(label='Nome da Câmera:', max_length = 50, widget=forms.TextInput(attrs={'placeholder': 'ex: Hall de Entrada.'}))
-    serie = forms.CharField(label='Número de Série:', max_length = 16, widget=forms.TextInput(attrs={'placeholder': 'Apenas caracteres maiúsculos e números, sem espaço.'}))
-    fabricante = forms.ChoiceField(label='Fabricante:', choices=FABRICANTES)
+    nome = forms.CharField(label='Nome da Câmera:', max_length = 50, widget=forms.TextInput(attrs={'placeholder': 'ex: Hall de Entrada.'}), required=True)
+    serie = SerieField(label='Número de Série:', max_length = 16, widget=forms.TextInput(attrs={'placeholder': 'Apenas caracteres maiúsculos e números, sem espaço.'}), required=True)
+    fabricante = forms.ChoiceField(widget = forms.Select(), label='Fabricante:', choices=FABRICANTES, required=True)
     
--- django/desafio/models.py
+++ django/desafio/models.py
@@ -4,15 +4,24 @@ from django import forms
 # Create your models here.
 
 
+
 class SerieField(models.CharField):
     def to_python(self, value):
         return value.upper()
 
 
 class Camera(models.Model):
+    FABRICANTES = (
+    ('Secure Câmeras Inc', 'Secure Câmeras Inc'),
+    ('Surveillance Cams LLC', 'Surveillance Cams LLC'),
+    ('DigiEye Group', 'DigiEye Group'),
+    ('CâmeraFi Inc', 'CâmeraFi Inc'),
+    ('VidMasters Inc', 'VidMasters Inc')
+    )
+
     nome      = models.CharField(max_length=50, default='nome')
-    fabricante = models.CharField(max_length=50, default='fabricante')
-    serie       = SerieField(max_length=16, default='serie', unique=True)
+    fabricante = models.CharField(max_length=50, default='fabricante', choices= FABRICANTES)
+    serie       = SerieField(max_length=16, default='serie')
 
 
 
