7dfa374333e7814bf1f16123db451bac52056ce6
larissabral <64551159+larissabral@users.noreply.github.com>
2020-06-19 12:07:51 -0300

    Inclusão da rota para API Rest

--- django/octos/urls.py
+++ django/octos/urls.py
@@ -15,8 +15,15 @@ Including another URLconf
 """
 from django.urls import path, include
 from django.contrib import admin
+from desafio.views import CameraViewSet
+from rest_framework import routers
+
+router = routers.DefaultRouter()
+router.register(r'cameras', CameraViewSet)
 
 urlpatterns = [
-   # path('admin/', admin.site.urls),
-    path('', include("desafio.urls"))
+    path('admin/', admin.site.urls),
+    path('', include("desafio.urls")),
+    path('', include(router.urls)),
+    path('api-auth/', include('rest_framework.urls'))
 ]
