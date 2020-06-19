1c9c4dd0141229b3cd7750a1448a788f0d1ef1bb
larissabral <64551159+larissabral@users.noreply.github.com>
2020-06-19 14:42:41 -0300

    Inclusão das urls para views da API Rest

--- django/desafio/urls.py
+++ django/desafio/urls.py
@@ -1,4 +1,5 @@
 from django.urls import path
+from rest_framework.urlpatterns import format_suffix_patterns
 from . import views
 
 app_name = "desafio"
@@ -9,4 +10,10 @@ urlpatterns = [
     path('list/', views.list, name='list'),
     path('remove/', views.remove, name='remove'),
     path('removeCamera/<int:id>', views.remove_camera, name='removeCamera'),
+
+#API 
+    path('cameras/', views.CameraList.as_view()),
+    path('cameras/<int:pk>/', views.CameraDetail.as_view()),
 ]
+
+urlpatterns = format_suffix_patterns(urlpatterns)
