ea96b0b9cae776392d4f1292e8041032abe5b19d
larissabral <64551159+larissabral@users.noreply.github.com>
2020-06-21 13:31:00 -0300

    Comentado caminho para acessar pagina admin

--- django/octos/urls.py
+++ django/octos/urls.py
@@ -17,7 +17,7 @@ from django.urls import path, include
 from django.contrib import admin
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+ #   path('admin/', admin.site.urls),
     path('', include("desafio.urls")),
 
 ]
